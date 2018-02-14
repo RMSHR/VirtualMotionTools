9c4d5527f00a5bd79204f4e0d46b80eed8c88712
Remy <remy.sohier@gmail.com>
2018-02-14 17:36:48 +0100

    v3
    Signed-off-by: Remy <remy.sohier@gmail.com>

---
+++ Assets/Scenes/Proto_ADSR/Scripts/ADSR_Structures.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ADSR_WaveStruct
+{
+    public float intensity;
+    public float delay;
+}
+
+public class ADSR_WaveShape
+{
+    public ADSR_WaveStruct attack;
+    public ADSR_WaveStruct delay;
+    public ADSR_WaveStruct sustain;
+    public ADSR_WaveStruct release;
+
+    public float intensity;
+    public float time;
+    public bool isReleasing = false;
+
+    public ADSR_WaveShape()
+    {
+
+    }
+}
---
+++ Assets/Scenes/Proto_ADSR/Scripts/ADSR_v3.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADSR_v3 : MonoBehaviour {
+
+    [Header("Attack")]
+    public float attack_intensity;
+    public float attack_delay;
+    [Header("Delay")]
+    public float delay_intensity;
+    public float delay_delay;
+    [Header("Sustain")]
+    public float sustain_intensity;
+    public float sustain_delay;
+    [Header("Release")]
+    public float release_intensity;
+    public float release_delay;
+
+    [Header("Entity Motion")]
+    public float speed = 1f;
+
+    private ADSR_WaveShape motionBehaviour;
+
+    void Awake()
+    {
+        motionBehaviour = new ADSR_WaveShape();
+
+        motionBehaviour.attack.intensity = attack_intensity;
+        motionBehaviour.attack.delay = attack_delay;
+
+        motionBehaviour.delay.intensity = delay_intensity;
+        motionBehaviour.delay.delay = delay_delay;
+
+        motionBehaviour.sustain.intensity = sustain_intensity;
+        motionBehaviour.sustain.delay = sustain_delay;
+
+        motionBehaviour.release.intensity = release_intensity;
+        motionBehaviour.release.delay = release_delay;
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h != 0f) // Input
+        {
+            if (motionBehaviour.isReleasing)
+            {
+                motionBehaviour.isReleasing = false;
+                motionBehaviour.time = 0f;
+            }
+                
+            motionBehaviour.time += Time.deltaTime;
+
+            if(motionBehaviour.time < motionBehaviour.attack.delay)
+            {
+                motionBehaviour.intensity = Mathf.MoveTowards(motionBehaviour.intensity, motionBehaviour.attack.intensity, motionBehaviour.time);
+                //motionBehaviour.intensity = Mathf.Clamp(motionBehaviour.intensity, 0f, motionBehaviour.attack.intensity); // security
+            }
+
+            if(motionBehaviour.time >= motionBehaviour.attack.delay && motionBehaviour.time < motionBehaviour.delay.delay)
+            {
+                motionBehaviour.intensity = Mathf.MoveTowards(motionBehaviour.intensity, motionBehaviour.delay.intensity, motionBehaviour.time + motionBehaviour.attack.intensity);
+            }
+
+            if(motionBehaviour.time >= motionBehaviour.delay.delay)
+            {
+                motionBehaviour.intensity = motionBehaviour.sustain.intensity;
+            }
+        }
+        else // Release
+        {
+            if(!motionBehaviour.isReleasing)
+            {
+                motionBehaviour.time = 0f;
+                motionBehaviour.isReleasing = true;
+            }
+
+            motionBehaviour.time += Time.deltaTime;
+
+            if (motionBehaviour.time < motionBehaviour.release.delay)
+            {
+                motionBehaviour.intensity = Mathf.MoveTowards(motionBehaviour.intensity, motionBehaviour.release.intensity, motionBehaviour.time);
+                motionBehaviour.intensity = Mathf.Clamp01(motionBehaviour.intensity);
+            }
+        }
+
+        transform.Translate(Vector3.right * Time.deltaTime * motionBehaviour.intensity * speed);
+    }
+}
